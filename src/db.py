6bec6ddc0e35421a0810a52cb3013fc02b76d18f
unknown <23f2000441@ds.study.iitm.ac.in>
2026-03-17 19:00:02 +0530

    Register with the Application

--- src/db.py
+++ src/db.py
@@ -22,6 +22,7 @@ def close_db(e=None):
     if db is not None:
         db.close()
 
+#Add the Python functions that will run these SQL commands to the db.py file:
 def init_db():
     db = get_db()
 
@@ -35,7 +36,11 @@ def init_db_command():
     init_db()
     click.echo('Initialized the database.')
 
-
+# Register with the Application
 sqlite3.register_converter(
     "timestamp", lambda v: datetime.fromisoformat(v.decode())
 )
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
